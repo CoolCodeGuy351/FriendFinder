d06d6ed35ecbac3645c4773bf243235e47961df1
Joe Mulick <joemulick@gmail.com>
2017-05-17 21:00:57 -0700

    Made major updates to server.js, server currently running. Filled out apiRoutes.js and htmlRoutes.js modules.

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var htmlRoute = require("./app/routing/htmlRoutes.js");
+var apiRoute = require("./app/routing/apiRoutes.js");
+
+var app = express();
+var PORT =process.env.PORT || 3020;
+
+// Sets up the Express app to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Import modules from htmlRoutes & apiRoutes
+htmlRoute(app);
+apiRoute(app);
+
+
+app.listen(PORT, () => console.log("App listening on port " + PORT));
